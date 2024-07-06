48a6c23eee80fa8b0b6f940a00c65f9c210af1c1
Sara Oliveira <saoliveira@deloitte.pt>
2024-07-06 16:01:35 +0100

    feat(FPHYSIO-31): added variables file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,17 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['normalize.css', '~/assets/styles/main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "~/assets/styles/_variables.scss" as *;
+          `,
+        },
+      },
+    },
+  },
   modules: [
     [
       '@nuxtjs/google-fonts',
