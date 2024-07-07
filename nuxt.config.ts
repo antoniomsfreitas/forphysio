c4b88900f4d410141a51a9c33d7b1998c83efe2a
Sara Oliveira <43679058+sararaqueloliveira@users.noreply.github.com>
2024-07-07 12:02:45 +0100

    feat(FPHYSIO-32): colors configuration (#5)
    - Colors variables configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['normalize.css', '~/assets/styles/main.scss'],
   vite: {
     css: {
       preprocessorOptions: {
