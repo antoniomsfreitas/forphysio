6ce6559338a1f23b6d916dd6c8ed143667c1f7b2
Sara Oliveira <43679058+sararaqueloliveira@users.noreply.github.com>
2024-07-07 12:01:13 +0100

    feat(FPHYSIO-31): font-family configuration (#3)
    - SASS/SCSS installation
    - Font-family configuration: **Roboto**

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['normalize.css'],
+  css: ['normalize.css', '~/assets/styles/main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "~/assets/styles/utils/_variables.scss" as *;
+          `,
+        },
+      },
+    },
+  },
+  modules: [
+    [
+      '@nuxtjs/google-fonts',
+      {
+        families: {
+          Roboto: [300, 400, 500],
+        },
+        preload: true,
+      },
+    ],
+  ],
 });
