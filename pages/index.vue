0d52e494a6ca5932a31447481f35f12efe31056e
Sara Oliveira <saoliveira@deloitte.pt>
2024-07-13 00:27:49 +0100

    feat(FPHYSIO-12): reset index vue file (homepage)

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,3 @@
 <template>
-  <div class="test">
-    <Gradient class="test__gradient" :direction="GradientDirections.BOTTOM_TOP" />
-  </div>
+  <h1>Homepage</h1>
 </template>
-
-<script setup lang="ts">
-import { GradientDirections } from '~/models/layout/gradient.model';
-</script>
-
-<style scoped lang="scss">
-.test {
-  position: relative;
-  width: 500px;
-  height: 500px;
-  background-color: $white;
-
-  &__gradient {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    height: 50%;
-  }
-}
-</style>
