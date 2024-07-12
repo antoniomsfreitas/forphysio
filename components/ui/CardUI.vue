9424f0041b7013df551f31467363e9ac016220a7
Sara Oliveira <saoliveira@deloitte.pt>
2024-07-13 00:25:45 +0100

    feat(FPHYSIO-12): component card adjustments and new linear-gradient mixin

---
+++ components/ui/CardUI.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="card">
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.card {
+  position: relative;
+  overflow: hidden;
+  border-radius: 15px;
+}
+</style>
--- models/layout/gradient.model.ts
+++
@@ -1,6 +0,0 @@
-export enum GradientDirections {
-  LEFT_RIGHT = 'left-right',
-  RIGHT_LEFT = 'right-left',
-  TOP_BOTTOM = 'top-bottom',
-  BOTTOM_TOP = 'bottom-top',
-}
