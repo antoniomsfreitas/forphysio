83d14d5d0c0f2d3a9a4af49b3d678acf253e4b73
António Freitas <geral@antoniofreitas.pt>
2024-07-12 20:42:27 +0100

    feat(FPHYSIO-12): gradient component first concept

---
+++ components/layout/Gradient.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="gradient" :class="`${direction}`" />
+</template>
+
+<script setup lang="ts">
+import { GradientDirections } from '~/models/layout/gradient.model';
+
+defineProps({
+  direction: {
+    type: String as PropType<GradientDirections>,
+    default: GradientDirections.BOTTOM_TOP,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.gradient {
+  &.top-bottom {
+    background: linear-gradient(180deg, $black 0%, rgb(0 0 0 / 0%) 100%);
+  }
+
+  &.bottom-top {
+    background: linear-gradient(0deg, $black 0%, rgb(0 0 0 / 0%) 100%);
+  }
+
+  &.left-right {
+    background: linear-gradient(90deg, $black 0%, rgb(0 0 0 / 0%) 100%);
+  }
+
+  &.right-left {
+    background: linear-gradient(270deg, $black 0%, rgb(0 0 0 / 0%) 100%);
+  }
+}
+</style>
