a70358722fa2f999d52e8bcae87485437d30b0d0
António Freitas <antfreitas@deloitte.pt>
2024-07-22 22:55:23 +0100

    feat(FPHYSIO-10): hero banner prototype

---
+++ components/blocks/homepage/HeroBannerBlock.vue
@@ -0,0 +1,23 @@
+<template>
+  <Swiper
+    class="hero-banner"
+    :modules="[SwiperAutoplay, SwiperScrollbar]"
+    :autoplay="{
+      delay: 4000,
+      disableOnInteraction: true,
+    }"
+    :slides-per-view="1"
+    :scrollbar="{ draggable: true }"
+  >
+    <SwiperSlide> Teste {{ windowHeight }} </SwiperSlide>
+    <SwiperSlide> Teste 2 </SwiperSlide>
+    <SwiperSlide> Teste 3 </SwiperSlide>
+    <SwiperSlide> Teste 4 </SwiperSlide>
+  </Swiper>
+</template>
+
+<style scoped lang="scss">
+.hero-banner {
+  height: 485px;
+}
+</style>
