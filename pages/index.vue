39be6e062329051500d293d3c50932075ca80a27
Sara Oliveira <43679058+sararaqueloliveira@users.noreply.github.com>
2024-08-01 17:32:43 +0100

    feat(FPHYSIO-14): two images block (#15)
    ![image](https://github.com/user-attachments/assets/6462c1d3-e257-4bbb-ae7e-85a85f38ce21)

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,38 @@
 <template>
   <div class="content">
     <HeroBannerBlock />
-    <AboutUsBlock />
+    <AboutUsBlock class="about-us-block" />
+    <TwoImagesBlock class="two-images-block" />
   </div>
 </template>
+
+<style scoped lang="scss">
+.about-us-block {
+  @include mq-mobile {
+    margin-top: 100px;
+    margin-bottom: 50px;
+  }
+
+  @include mq-tablet {
+    margin-top: 120px;
+    margin-bottom: 80px;
+  }
+
+  @include mq-desktop {
+    margin-top: 130px;
+    margin-bottom: 100px;
+  }
+}
+
+.two-images-block {
+  margin-bottom: 100px;
+
+  @include mq-mobile-tablet {
+    margin-top: 80px;
+  }
+
+  @include mq-desktop {
+    margin-top: 100px;
+  }
+}
+</style>
