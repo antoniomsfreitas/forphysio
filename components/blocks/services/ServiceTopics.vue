c983326b2c6aaf10985e6d2a54d768f984991190
Sara Oliveira <43679058+sararaqueloliveira@users.noreply.github.com>
2024-10-13 22:53:08 +0100

    chore(FPHYSIO-84): Services api update (#52)

--- components/blocks/services/ServiceTopics.vue
+++ components/blocks/services/ServiceTopics.vue
@@ -4,7 +4,11 @@
       <LayoutGridRow>
         <LayoutGridCol m="4" t="10" d="6" start-col-t="2" start-col-d="2">
           <h2 v-t="data.title" />
+        </LayoutGridCol>
+      </LayoutGridRow>
 
+      <LayoutGridRow>
+        <LayoutGridCol m="4" t="10" d="12" start-col-t="2" start-col-d="2">
           <div class="topics-list--topics" v-html="data.topics"></div>
         </LayoutGridCol>
       </LayoutGridRow>
@@ -38,25 +42,39 @@ defineProps({
     }
   }
 
-  :deep(ul) {
-    li {
+  &--topics {
+    @include mq-desktop {
       display: flex;
-      color: $medium-grey;
-      font-weight: $font-weight-light;
-      line-height: 1.2;
-      padding-bottom: 12px;
+      justify-content: space-between;
+      gap: 24px;
+    }
 
-      @include mq-mobile-tablet {
-        font-size: 24px;
+    :deep(ul) {
+      li {
+        display: flex;
+        color: $medium-grey;
+        font-weight: $font-weight-light;
+        line-height: 1.2;
+        padding-bottom: 12px;
+
+        @include mq-mobile-tablet {
+          font-size: 24px;
+        }
+
+        @include mq-desktop {
+          font-size: 32px;
+        }
+
+        &::before {
+          content: '\2022';
+          padding: 0 10px;
+        }
       }
 
-      @include mq-desktop {
-        font-size: 32px;
-      }
-
-      &::before {
-        content: '\2022';
-        padding: 0 10px;
+      &:nth-child(2) {
+        @include mq-desktop {
+          margin-top: 495px;
+        }
       }
     }
   }
