5f65f1b1b0c8363ab7e9445ef186e9f10cf36d35
Sara Oliveira <sara.raqueloliveira@hotmail.com>
2024-10-14 15:26:32 +0100

    chore(FPHYSIO-83): About us page API

---
+++ server/data/about.ts
@@ -0,0 +1,171 @@
+export const data = {
+  introBlock: {
+    topics: [
+      {
+        title: {
+          translations: {
+            pt: 'Missão',
+            en: 'Mission',
+            es: 'Misión',
+            'zh-sg': '使命',
+          },
+        },
+        text: {
+          translations: {
+            pt: 'Prevenir o risco de lesão. Reabilitar a função.\nOtimizar a performance.',
+            en: 'Prevent injury risk. Rehabilitate function.\nOptimize performance.',
+            es: 'Prevenir el riesgo de lesión. Rehabilitar la función.\nOptimizar el rendimiento.',
+            'zh-sg': '预防受伤风险。 恢复功能。\n优化表现。',
+          },
+        },
+      },
+      {
+        title: {
+          translations: {
+            pt: 'Visão',
+            en: 'Vision',
+            es: 'Visión',
+            'zh-sg': '愿景',
+          },
+        },
+        text: {
+          translations: {
+            pt: 'Proporcionar os melhores cuidados aos nossos clientes, minimizar os períodos de recuperação e otimizar os resultados de intervenção.',
+            en: 'Provide the best care for our clients, minimize recovery periods, and optimize intervention outcomes.',
+            es: 'Brindar el mejor cuidado a nuestros clientes, minimizar los periodos de recuperación y optimizar los resultados de intervención.',
+            'zh-sg': '为客户提供最佳护理，缩短康复期并优化干预结果。',
+          },
+        },
+      },
+      {
+        title: {
+          translations: {
+            pt: 'Valores',
+            en: 'Values',
+            es: 'Valores',
+            'zh-sg': '价值观',
+          },
+        },
+        text: {
+          translations: {
+            pt: 'Qualidade. Ciência. Individualidade.',
+            en: 'Quality. Science. Individuality.',
+            es: 'Calidad. Ciencia. Individualidad.',
+            'zh-sg': '质量。 科学。 个性。',
+          },
+        },
+      },
+    ],
+    image: {
+      mobile: '/images/about-us/intro/image-mobile.jpg',
+      tablet: '/images/about-us/intro/image-tablet.jpg',
+      desktop: '/images/about-us/intro/image-desktop.jpg',
+      alt: {
+        translations: {
+          pt: 'Imagem de alguns membros da equipa da Forphysio.',
+          en: 'Image of some members of the Forphysio team.',
+          es: 'Imagen de algunos miembros del equipo de Forphysio.',
+          'zh-sg': 'Forphysio团队部分成员的照片。',
+        },
+      },
+    },
+  },
+
+  textBlock_1: {
+    title: {
+      translations: {
+        pt: 'O nosso conceito',
+        en: 'Our concept',
+        es: 'Nuestro concepto',
+        'zh-sg': '我们的理念',
+      },
+    },
+    textCol1: {
+      translations: {
+        pt: 'As sessões individualizadas na ForPhysio Clinic, com 60 minutos de duração e sempre na presença do profissional de saúde, garantem melhores resultados, de forma mais rápida e segura.\n\nAcreditamos que cada pessoa deverá assumir um papel ativo na melhoria da sua condição clínica e na promoção da longevidade, sempre guiado de forma próxima por profissionais especializados.\n\nAo longo dos dez anos da ForPhysio Clinic, temos aprimorado estratégias de avaliação clínica e inovado nos programas de recuperação e melhoria da funcionalidade. Cada sessão é guiada através de registos clínicos objetivos que permitem medir a evolução dos clientes e progredir com segurança.',
+        en: "Individualized sessions at ForPhysio Clinic, lasting 60 minutes and always in the presence of a healthcare professional, ensure better results more quickly and safely.\n\nWe believe that each person should take an active role in improving their clinical condition and promoting longevity, always closely guided by specialized professionals.\n\nThroughout ForPhysio Clinic's ten years, we have refined clinical evaluation strategies and innovated recovery and functionality improvement programs. Each session is guided by objective clinical records, allowing us to measure clients' progress and advance safely.",
+        es: 'Las sesiones individualizadas en ForPhysio Clinic, con una duración de 60 minutos y siempre en presencia de un profesional de la salud, garantizan mejores resultados, de manera más rápida y segura.\n\nCreemos que cada persona debe asumir un papel activo en la mejora de su condición clínica y en la promoción de la longevidad, siempre guiada de cerca por profesionales especializados.\n\nA lo largo de los diez años de ForPhysio Clinic, hemos mejorado las estrategias de evaluación clínica e innovado en los programas de recuperación y mejora de la funcionalidad. Cada sesión se guía a través de registros clínicos objetivos que permiten medir la evolución de los clientes y progresar con seguridad.',
+        'zh-sg':
+          'ForPhysio Clinic的个性化会诊持续60分钟，并且总是在医疗专业人员的陪同下进行，确保更快、更安全地取得更好效果。\n\n我们相信，每个人都应该在改善临床状况和促进长寿方面发挥积极作用，始终由专业人员紧密指导。\n\n在ForPhysio Clinic的十年中，我们 refined了临床评估策略，并创新了恢复和功能改进计划。每个会诊都由客观的临床记录引导，让我们能够测量客户的进展并安全地推进。',
+      },
+    },
+    textCol2: {
+      translations: {
+        pt: 'As nossas Unidades de Saúde integram um ambiente moderno e sofisticado, com tecnologia e equipamentos de excelência, a pensar na melhor experiência de cada cliente.\n\nPara prestar o melhor acompanhamento fora de portas, facilitamos ainda canais de comunicação online, que colocam em comunicação direta os profissionais da ForPhysio Clinic e os nossos clientes. Assim, alterar consultas, colocar dúvidas ou fazer pedidos de informação é mais rápido e eficiente.\n\nNa ForPhysio Clinic prestamos serviços de saúde atuais, à sua medida e que realmente importam. Estamos disponíveis para o receber nas nossas Unidades de Saúde.',
+        en: 'Our Health Units offer a modern and sophisticated environment, with state-of-the-art technology and equipment, designed to provide the best experience for each client.\n\nTo offer better support outside our facilities, we also provide online communication channels that put ForPhysio Clinic professionals in direct contact with our clients. This makes rescheduling appointments, asking questions, or requesting information faster and more efficient.\n\nAt ForPhysio Clinic, we provide up-to-date healthcare services, tailored to your needs, and that truly matter. We are ready to welcome you at our Health Units.',
+        es: 'Nuestras Unidades de Salud integran un entorno moderno y sofisticado, con tecnología y equipos de excelencia, pensando en la mejor experiencia de cada cliente.\n\nPara brindar el mejor seguimiento fuera de nuestras instalaciones, también facilitamos canales de comunicación en línea, que ponen en contacto directo a los profesionales de ForPhysio Clinic con nuestros clientes. Así, cambiar citas, hacer preguntas o solicitar información es más rápido y eficiente.\n\nEn ForPhysio Clinic, brindamos servicios de salud actualizados, a su medida y que realmente importan. Estamos disponibles para recibirle en nuestras Unidades de Salud.',
+        'zh-sg':
+          '我们的健康中心提供现代化且先进的环境，配备尖端技术和设备，旨在为每位客户提供最佳体验。\n\n为了在设施外提供更好的支持，我们还提供在线沟通渠道，使ForPhysio Clinic的专业人员与客户直接联系。这使得重新安排预约、提问或请求信息变得更加快捷和高效。\n\n在ForPhysio Clinic，我们提供符合您需求的最新医疗服务，并且真正关心您的健康。我们随时欢迎您到我们的健康中心。',
+      },
+    },
+  },
+
+  imageFullwidth: {
+    mobile: '/images/about-us/content/image-1/image-1-mobile.jpg',
+    tablet: '/images/about-us/content/image-1/image-1-tablet.jpg',
+    desktop: '/images/about-us/content/image-1/image-1-desktop.jpg',
+    alt: {
+      translations: {
+        pt: 'Imagem de alguns membros da equipa da Forphysio.',
+        en: 'Image of some members of the Forphysio team.',
+        es: 'Imagen de algunos miembros del equipo de Forphysio.',
+        'zh-sg': 'Forphysio团队一些成员的照片.',
+      },
+    },
+  },
+
+  textBlock_2: {
+    title: {
+      translations: {
+        pt: 'A nossa história',
+        en: 'Our History',
+        es: 'Nuestra Historia',
+        'zh-sg': '我们的历史',
+      },
+    },
+    textCol1: {
+      translations: {
+        pt: 'A ForPhysio Clinic é uma empresa portuguesa que presta serviços de fisioterapia desde Outubro de 2014.\n\nDe forma a diferenciar a sua oferta de serviços, o fisioterapeuta Nuno Pina, fundador da ForPhysio Clinic e atual diretor, identificou a necessidade de criar serviços de fisioterapia que permitissem minimizar os períodos de recuperação e melhorar os resultados após intervenção clínica.',
+        en: 'ForPhysio Clinic is a Portuguese company that has been providing physiotherapy services since October 2014.\n\nIn order to differentiate its service offerings, physiotherapist Nuno Pina, founder and current director of ForPhysio Clinic, identified the need to create physiotherapy services that would minimize recovery times and improve results after clinical intervention.',
+        es: 'ForPhysio Clinic es una empresa portuguesa que ha estado proporcionando servicios de fisioterapia desde octubre de 2014.\n\nPara diferenciar su oferta de servicios, el fisioterapeuta Nuno Pina, fundador y actual director de ForPhysio Clinic, identificó la necesidad de crear servicios de fisioterapia que permitieran minimizar los tiempos de recuperación y mejorar los resultados tras la intervención clínica.',
+        'zh-sg':
+          'ForPhysio Clinic是一家自2014年10月以来提供物理治疗服务的葡萄牙公司。\n\n为了使其服务与众不同，ForPhysio Clinic的创始人兼现任主任Nuno Pina理疗师认识到有必要创建一种物理治疗服务，以尽量缩短恢复时间并改善临床干预后的效果。',
+      },
+    },
+    textCol2: {
+      translations: {
+        pt: 'Desde esse momento que a ForPhysio Clinic tem evoluído consistentemente. Além do serviço de fisioterapia foram adicionados serviços complementares, de nutrição, performance e bem-estar. A coordenação do departamento de fisioterapia da Federação Portuguesa de Natação entre 2016 e 2020, é um marco na história da empresa.',
+        en: 'Since then, ForPhysio Clinic has consistently evolved. In addition to physiotherapy services, complementary services in nutrition, performance, and wellness have been added. Coordinating the physiotherapy department of the Portuguese Swimming Federation from 2016 to 2020 is a milestone in the company’s history.',
+        es: 'Desde ese momento, ForPhysio Clinic ha evolucionado consistentemente. Además de los servicios de fisioterapia, se han añadido servicios complementarios en nutrición, rendimiento y bienestar. La coordinación del departamento de fisioterapia de la Federación Portuguesa de Natación entre 2016 y 2020 es un hito en la historia de la empresa.',
+        'zh-sg':
+          '自那时以来，ForPhysio Clinic不断发展。除了物理治疗服务外，还增加了营养、运动表现和健康方面的辅助服务。2016年至2020年期间协调葡萄牙游泳联合会物理治疗部门是公司历史上的一个里程碑。',
+      },
+    },
+  },
+
+  sideImage: {
+    mobile: '/images/about-us/content/image-2/image-2-mobile.jpg',
+    tablet: '/images/about-us/content/image-2/image-2-tablet.jpg',
+    desktop: '/images/about-us/content/image-2/image-2-desktop.jpg',
+    alt: {
+      translations: {
+        pt: 'Imagem de alguns membros da equipa da Forphysio.',
+        en: 'Image of some members of the Forphysio team.',
+        es: 'Imagen de algunos miembros del equipo de Forphysio.',
+        'zh-sg': 'Forphysio团队部分成员的照片.',
+      },
+    },
+  },
+
+  textBlock_3: {
+    text: {
+      translations: {
+        pt: 'Aos dias de hoje, a ForPhysio conta com mais de dez parceiros em diversas áreas, e uma equipa de vinte profissionais que colaboram em 2 unidades próprias e 3 unidades parceiras, tornando possível o acesso a serviços de excelência.',
+        en: 'To date, ForPhysio has over ten partners in various fields, and a team of twenty professionals collaborating in 2 own units and 3 partner units, making access to excellent services possible.',
+        es: 'Hasta la fecha, ForPhysio cuenta con más de diez socios en varias áreas, y un equipo de veinte profesionales que colaboran en 2 unidades propias y 3 unidades asociadas, lo que hace posible el acceso a servicios de excelencia.',
+        'zh-sg':
+          '截至目前，ForPhysio在多个领域拥有超过十个合作伙伴，并有一支由二十名专业人员组成的团队，在两家自有单位和三家合作单位中合作，使得能够提供卓越的服务。',
+      },
+    },
+  },
+};
