14fe220a6d9ea0000bf56bef0b0a4575ed4de58b
Sara Oliveira <43679058+sararaqueloliveira@users.noreply.github.com>
2024-10-16 23:28:27 +0100

    chore(FPHYSIO-81): layout api (#55)

---
+++ server/data/layout/footer.ts
@@ -0,0 +1,59 @@
+import { Routes } from '~/models/routes.model';
+
+export const data = {
+  mainMenu: [
+    {
+      name: 'pages.services',
+      route: { name: Routes.SERVICES },
+    },
+    {
+      name: 'pages.about',
+      route: { name: Routes.ABOUT },
+    },
+    {
+      name: 'pages.team',
+      route: { name: Routes.TEAM },
+    },
+    {
+      name: 'pages.partnerships',
+      route: { name: Routes.PARTNERSHIPS },
+    },
+    {
+      name: 'pages.blog',
+      route: { name: Routes.BLOG },
+    },
+    {
+      name: 'pages.contacts',
+      route: { name: Routes.CONTACTS },
+    },
+    {
+      name: 'pages.units',
+      route: { name: Routes.HOMEPAGE },
+    },
+    {
+      name: 'pages.cancellation-policy',
+      route: { name: Routes.HOMEPAGE },
+    },
+    {
+      name: 'pages.privacy-policy',
+      route: { name: Routes.HOMEPAGE },
+    },
+    {
+      name: 'pages.faqs',
+      route: { name: Routes.FAQ },
+    },
+  ],
+
+  logosMenu: [
+    {
+      name: 'general.complaintsBook',
+      link: 'https://www.livroreclamacoes.pt/Inicio/',
+      image: 'livro_reclamacoes.svg',
+    },
+    {
+      name: 'general.complimentsBook',
+      link: 'https://livrodeelogios.com/',
+      image: 'livro_elogios.svg',
+    },
+  ],
+};
