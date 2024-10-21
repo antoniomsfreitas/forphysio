74f64534339817663128faf29179e19bc12bd450
António Freitas <geral@antoniofreitas.pt>
2024-10-21 22:45:18 +0100

    feat(FPHYSIO-17): content height 100%

--- components/blocks/blog/ArticleHighlightHomepage.vue
+++ components/blocks/blog/ArticleHighlightHomepage.vue
@@ -51,188 +51,196 @@ const btnSize = computed(() => (isMobile.value ? 'small' : 'medium'));
 </script>
 
 <style scoped lang="scss">
-.image {
-  position: absolute;
-  top: 0;
-  left: 0;
+.blog-highlighted-article {
+  display: flex;
   height: 100%;
-  transition: left 800ms ease 400ms;
 
-  @include mq-desktop {
-    width: 55vw;
+  .image {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    transition: left 800ms ease 400ms;
+
+    @include mq-desktop {
+      width: 55vw;
+    }
+
+    @include mq-mobile-tablet {
+      width: 100%;
+    }
+
+    &__picture {
+      position: relative;
+      height: 100%;
+
+      @include mq-desktop {
+        border-radius: 0 15px 15px 0;
+      }
+    }
+
+    &:deep(img) {
+      height: 100%;
+    }
+
+    &__gradient {
+      @include gradient-overlay('left-right', 100);
+    }
   }
 
-  @include mq-mobile-tablet {
-    width: 100%;
-  }
-
-  &__picture {
+  .content {
     position: relative;
+    z-index: 2;
+    width: 100%;
     height: 100%;
 
-    @include mq-desktop {
-      border-radius: 0 15px 15px 0;
-    }
-  }
-
-  &:deep(img) {
-    height: 100%;
-  }
-
-  &__gradient {
-    @include gradient-overlay('left-right', 100);
-  }
-}
-
-.content {
-  position: relative;
-  z-index: 2;
-  width: 100%;
-  height: 100%;
-
-  &__col {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-
-    @include mq-desktop {
-      gap: 118px;
-      padding: 152px 0 100px;
-    }
-
-    @include mq-tablet {
-      gap: 110px;
-      padding: 100px 0 154px;
-    }
-
-    @include mq-mobile {
-      gap: 40px;
-      padding: 70px 0 154px;
-    }
-  }
-
-  &__title {
-    display: block;
-    color: $white;
-  }
-
-  &__categories {
-    margin-bottom: 16px;
-
-    li {
+    &__col {
       display: flex;
-      gap: 6px;
-      align-items: center;
-      font-weight: $font-weight-semi-bold;
-      letter-spacing: 0.4px;
+      flex-direction: column;
+      justify-content: space-between;
+      height: 100%;
 
-      @include mq-tablet-desktop {
-        font-size: 20px;
+      @include mq-desktop {
+        display: flex;
+        gap: 118px;
+        height: 100%;
+        padding: 152px 0 100px;
+      }
+
+      @include mq-tablet {
+        gap: 110px;
+        padding: 100px 0 154px;
       }
 
       @include mq-mobile {
-        font-size: 16px;
+        gap: 40px;
+        padding: 70px 0 154px;
       }
-
-      &:not(:last-child)::after {
-        font-size: 14px;
-        color: $white;
-        content: '\2022';
-      }
-    }
-  }
-
-  &__titles {
-    display: flex;
-    flex-direction: column;
-    letter-spacing: 1px;
-
-    @include mq-tablet-desktop {
-      margin-bottom: 40px;
-    }
-
-    @include mq-mobile {
-      margin-bottom: 32px;
     }
 
     &__title {
-      font-weight: $font-weight-semi-bold;
+      display: block;
+      color: $white;
+    }
 
-      @include mq-desktop {
-        font-size: 86px;
-      }
+    &__categories {
+      margin-bottom: 16px;
 
-      @include mq-tablet {
-        font-size: 64px;
-      }
+      li {
+        display: flex;
+        gap: 6px;
+        align-items: center;
+        font-weight: $font-weight-semi-bold;
+        letter-spacing: 0.4px;
 
-      @include mq-mobile {
-        font-size: 36px;
+        @include mq-tablet-desktop {
+          font-size: 20px;
+        }
+
+        @include mq-mobile {
+          font-size: 16px;
+        }
+
+        &:not(:last-child)::after {
+          font-size: 14px;
+          color: $white;
+          content: '\2022';
+        }
       }
     }
 
-    &__subtitle {
-      font-weight: $font-weight-thin;
+    &__titles {
+      display: flex;
+      flex-direction: column;
+      letter-spacing: 1px;
 
-      @include mq-desktop {
-        font-size: 48px;
-      }
-
-      @include mq-tablet {
-        font-size: 36px;
+      @include mq-tablet-desktop {
+        margin-bottom: 40px;
       }
 
       @include mq-mobile {
+        margin-bottom: 32px;
+      }
+
+      &__title {
+        font-weight: $font-weight-semi-bold;
+
+        @include mq-desktop {
+          font-size: 86px;
+        }
+
+        @include mq-tablet {
+          font-size: 64px;
+        }
+
+        @include mq-mobile {
+          font-size: 36px;
+        }
+      }
+
+      &__subtitle {
+        font-weight: $font-weight-thin;
+
+        @include mq-desktop {
+          font-size: 48px;
+        }
+
+        @include mq-tablet {
+          font-size: 36px;
+        }
+
+        @include mq-mobile {
+          font-size: 18px;
+        }
+      }
+    }
+
+    &__intro {
+      font-weight: $font-weight-light;
+      line-height: 1.2;
+
+      @include mq-tablet-desktop {
+        max-width: 360px;
+        margin-bottom: 40px;
         font-size: 18px;
       }
-    }
-  }
 
-  &__intro {
-    font-weight: $font-weight-light;
-    line-height: 1.2;
-
-    @include mq-tablet-desktop {
-      max-width: 360px;
-      margin-bottom: 40px;
-      font-size: 18px;
+      @include mq-mobile {
+        width: 100%;
+        max-width: 280px;
+        margin-bottom: 32px;
+        font-size: 16px;
+      }
     }
 
-    @include mq-mobile {
-      width: 100%;
-      max-width: 280px;
-      margin-bottom: 32px;
-      font-size: 16px;
-    }
-  }
+    &__author {
+      @include mq-tablet-desktop {
+        margin-bottom: 40px;
+      }
 
-  &__author {
-    @include mq-tablet-desktop {
-      margin-bottom: 40px;
-    }
+      @include mq-mobile {
+        margin-bottom: 32px;
+      }
 
-    @include mq-mobile {
-      margin-bottom: 32px;
+      :deep(img) {
+        background-color: $border-grey;
+      }
     }
 
-    :deep(img) {
-      background-color: $border-grey;
-    }
-  }
+    &__button {
+      display: inline-block;
 
-  &__button {
-    display: inline-block;
+      @include mq-tablet-desktop {
+        min-width: 268px;
+      }
 
-    @include mq-tablet-desktop {
-      min-width: 268px;
-    }
+      @include mq-mobile {
+        min-width: 180px;
+      }
 
-    @include mq-mobile {
-      min-width: 180px;
-    }
-
-    :deep(button) {
-      width: 100%;
+      :deep(button) {
+        width: 100%;
+      }
     }
   }
 }
