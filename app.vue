b82a0f4120b4fcfc78ac983a430f381a0f26fdb4
Sara Oliveira <43679058+sararaqueloliveira@users.noreply.github.com>
2024-10-24 11:09:53 +0100

    feat(FPHYSIO-98): favicon (#75)

--- app.vue
+++ app.vue
@@ -11,6 +11,32 @@ const isLayoutDataLoaded = ref(false);
 const isPageDataLoaded = ref(false);
 const isAppReady = computed(() => isLayoutDataLoaded.value && isPageDataLoaded.value);
 
+useHead({
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon/favicon-48x48-white.png',
+      sizes: '48x48',
+      media: '(prefers-color-scheme: dark)',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon/favicon-48x48.png',
+      sizes: '48x48',
+      media: '(prefers-color-scheme: light)',
+    },
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon-white.ico', media: '(prefers-color-scheme: dark)' },
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico', media: '(prefers-color-scheme: light)' },
+    { rel: 'icon', type: 'image/svg+xml', href: '/favicon/favicon-white.svg', media: '(prefers-color-scheme: dark)' },
+    { rel: 'icon', type: 'image/svg+xml', href: '/favicon/favicon.svg', media: '(prefers-color-scheme: light)' },
+    { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
+    { rel: 'manifest', href: '/favicon/site.webmanifest' },
+  ],
+  meta: [{ name: 'apple-mobile-web-app-title', content: 'ForPhysio' }],
+});
+
 const handleLayoutDataLoaded = () => {
   isLayoutDataLoaded.value = true;
 };
